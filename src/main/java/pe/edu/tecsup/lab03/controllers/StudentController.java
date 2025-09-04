ff4e158774bc902ebc6b774028fa400bd80c1667
steven <steven.rosas@tecsup.edu.pe>
2025-09-03 20:59:56 -0500

    Colaborador: agregadas clases Student etc

---
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -0,0 +1,7 @@
+package pe.edu.tecsup.lab03.controllers;
+
+public class StudentController {
+    public void index() {
+        System.out.println("StudentController funcionando...");
+    }
+}
---
+++ src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
@@ -0,0 +1,7 @@
+package pe.edu.tecsup.lab03.services;
+
+public class StudentService {
+    public void getStudents() {
+        System.out.println("StudentService obteniendo estudiantes...");
+    }
+}
