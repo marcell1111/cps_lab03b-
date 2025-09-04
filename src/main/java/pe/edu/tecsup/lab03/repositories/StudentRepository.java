0562f7fd85c0c0758802849640aeed1ad80dc6ee
steven <steven.rosas@tecsup.edu.pe>
2025-09-03 21:03:43 -0500

    Colaborador: agregadas clases Student etc

--- src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.tecsup.lab03.repositories;
-
-public class StudentRepository {
-    public void findById(int id) {
-        System.out.println("StudentRepository buscando estudiante con ID: " + id);
-    }
-}
--- src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.tecsup.lab03.services;
-
-public class StudentService {
-    public void getStudents() {
-        System.out.println("StudentService obteniendo estudiantes...");
-    }
-}
